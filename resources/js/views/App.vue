d5385a30f437c51c9767cfaa7273f69d86ad1e2f
olga_demina <olga.demina1410@gmail.com>
2022-07-06 10:34:41 +0200

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="container text-center mt-4">
+      <h1>Questa è la pagina guest di boolpress</h1>
+      <WorkInProgress />
+    </div>
+  </div>
+</template>
+
+<script>
+import WorkInProgress from "../components/WorkInProgress.vue";
+
+export default {
+  name: "App",
+  components: {
+    WorkInProgress,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h2> Ciao {{ $user->name }}! Benvenuto nella dashboard Boolpress</h2>
+@endsection
