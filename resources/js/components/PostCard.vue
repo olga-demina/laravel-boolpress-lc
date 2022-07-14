c439bf8e8705c9d0e70263cc0c2afeacd9896564
olga_demina <olga.demina1410@gmail.com>
2022-07-14 12:15:13 +0200

    Realizzata pagina di singolo post

--- resources/js/components/PostCard.vue
+++ resources/js/components/PostCard.vue
@@ -8,11 +8,9 @@
         {{ troncateText(50) }}
       </p>
     </div>
-    <!-- 
-          <div class="card-body">
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-          </div> -->
+    <div class="card-body">
+      <router-link :to="{ name: 'single-post', params: { slug: post.slug } }" class="card-link">Leggi post</router-link>
+    </div>
   </div>
 </template>
 
---
+++ resources/js/pages/SinglePost.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <section v-if="post">
+      <h1>{{ post.title }}</h1>
+      <p>Category: {{ categoryName }}</p>
+      <div class="mb-4">
+        <span
+          v-for="tag in post.tags"
+          :key="tag.id"
+          class="badge rounded-pill bg-warning text-dark mr-3"
+          >{{ tag.name }}</span
+        >
+      </div>
+      <p>
+        {{ post.content }}
+      </p>
+    </section>
+    <section v-else>
+        <h2>Loading...</h2>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SinglePost",
+  data() {
+    return {
+      post: null,
+    };
+  },
+  created() {
+    this.getPostDetails();
+  },
+  computed: {
+    categoryName() {
+      return this.post.category ? this.post.category.name : "nessuna";
+    },
+  },
+  methods: {
+    getPostDetails() {
+      // Prendere lo slug dalla route di vue
+      const slug = this.$route.params.slug;
+      // Fare la chiamata axios all'api, aggiungendo lo slug all'url dell'api
+      axios.get(`/api/posts/${slug}`).then((resp) => {
+        if (resp.data.success) {
+          this.post = resp.data.results;
+        } else {
+          this.$router.push({ name: "not-found" });
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
